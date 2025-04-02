aba37e0e7119d6d970ec461221556443323ddcbc
sferran2 <sferran@byupathway.edu>
2025-04-02 18:20:12 -0400

    Final Project

---
+++ Project_MrCookie/scripts/site_plan.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('current-year').textContent = currentYear;
+const currentDate = new Date();
+const formattedDate = currentDate.toLocaleString();
+document.querySelector('.last-modified').textContent = `Last Access: ${formattedDate}`;
